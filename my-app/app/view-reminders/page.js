5e4f08e39ac75c1e94745b09242883fcc7564880
MichaelC609 <mjcastillo@cpp.edu>
2025-04-19 09:37:44 -0700

    added js for calendar view

--- my-app/app/view-reminders/page.js
+++ my-app/app/view-reminders/page.js
@@ -1,7 +1,30 @@
 'use client';
+<<<<<<< Updated upstream
 import { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient.js';
 import Navbar from '../navbar';
+=======
+import {  useState, useEffect } from 'react';
+import { supabase } from '../supabaseClient.js'; 
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import format from 'date-fns/format';
+import parse from 'date-fns/parse';
+import startOfWeek from 'date-fns/startOfWeek';
+import getDay from 'date-fns/getDay';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const locales = {
+    'en-US': require('date-fns/locale/en-US'),
+};
+
+const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+});
+>>>>>>> Stashed changes
 
 export default function ViewReminders() {
     const [reminders, setReminders] = useState([]);
@@ -21,8 +44,22 @@ export default function ViewReminders() {
 
     }, []);
 
+<<<<<<< Updated upstream
     return (<>
         <Navbar className="navbar" />
+=======
+    const remindersToEvents = (reminders) => {
+        const weekdays = {
+            Mon: 1, Tue: 2, Wed: 3, Thu: 4, Fri: 5, Sat: 6, Sun: 0,
+        };
+
+        const today = new Date();
+        const baseDay = startOfWeek(today, { weekStartsOn: 0 });
+        const events = [];
+    };
+
+    return(
+>>>>>>> Stashed changes
         <div className="p-4">
             <h2 className="text-2xl font-bold mb-4">Medication Schedule</h2>
 
