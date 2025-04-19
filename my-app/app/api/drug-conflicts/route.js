d76b03218870564f40efd92b0ef2d1186705ffe8
joestrada1022 <student.joshuaestrada@gmail.com>
2025-04-19 12:44:02 -0700

    fix path and packages

--- my-app/app/api/drug-conflicts/route.js
+++ my-app/app/api/drug-conflicts/route.js
@@ -1,29 +1,29 @@
-import getDrugConflicts from "@/app/util/conflictsScraper";
+import getDrugConflicts from "../../util/conflictsScraper";
 
 
-export async function POST(req, res){
-    const body = await req.json();
-    const { drugName } = body;
+export async function POST(req, res) {
+  const body = await req.json();
+  const { drugName } = body;
 
-    
-    if (!drugName) {
-      return new Response(JSON.stringify({ error: "Drug name is required" }), {
-        status: 400,
-      });
-    }
 
-    try {
-        const results = await getDrugConflicts(drugName);
-        return new Response(JSON.stringify({ results }), {
-          status: 200,
-          headers: { "Content-Type": "application/json" },
-        });
-    } catch (error) {
-      console.error("Scraping failed:", error);
-      return new Response(JSON.stringify({ error: "Scraping failed" }), {
-        status: 500,
-        headers: { "Content-Type": "application/json" },
-      });
-    }
-    
+  if (!drugName) {
+    return new Response(JSON.stringify({ error: "Drug name is required" }), {
+      status: 400,
+    });
+  }
+
+  try {
+    const results = await getDrugConflicts(drugName);
+    return new Response(JSON.stringify({ results }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Scraping failed:", error);
+    return new Response(JSON.stringify({ error: "Scraping failed" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
 }
